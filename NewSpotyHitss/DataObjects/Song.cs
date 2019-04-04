0fac52308b83431f52be77bd127a53ff8576f6ce
carrascoja <carrascoja@HGDLAPCARRASCOJ>
2019-04-04 17:17:04 -0600

    Se cambiaron Carpetas a proyectos vacios y se oprganizan clases

---
+++ NewSpotyHitss/DataObjects/Song.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewSpotyHitss
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+    }
+}
