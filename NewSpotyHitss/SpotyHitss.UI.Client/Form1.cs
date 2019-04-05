d153b96ce3f922ff38bd85be0246df72c1b2e601
ricmtz <ricmtz15@gmail.com>
2019-04-05 15:49:25 -0600

    UI for created

---
+++ NewSpotyHitss/SpotyHitss.UI.Client/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpotyHitss.UI.Client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInsertSong_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearchByArtist_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInsertAlbum_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearchGenre_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
