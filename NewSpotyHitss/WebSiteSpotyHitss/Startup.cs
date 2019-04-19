40c682dc7c8c72aada842a7e52db288d9438aedc
carrascoja <carrascoja@HGDLAPCARRASCOJ>
2019-04-19 10:21:09 -0500

    Create new webpage named WebSiteSpotyHitss

---
+++ NewSpotyHitss/WebSiteSpotyHitss/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebSiteSpotyHitss.Startup))]
+namespace WebSiteSpotyHitss
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
